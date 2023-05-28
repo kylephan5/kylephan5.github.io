29e656d3e2b5574e2ba5facb023f43eae19a74b8
Kyle Phan <kyledphan@gmail.com>
2023-05-28 00:06:50 -0700

    Add styling for app, add navbar, home, and about to app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import React, {useState} from 'react';
 
 function App() {
+  const [currently, setCurrently] = useState();
+    
+  function WhatAmIDoing(i) {
+      let tasks = ["resting for a good day tomorrow 🤓", "playing Catan 🎲", "stuffing my face with food 🍲", "watching the Clippers 🏀", "learning Swift 📱", "in NYC!!! 🏙️", "resting for a good day tomorrow 🤓"];
+      setCurrently(() => tasks[i]);
+  }
+  var d = new Date();
+  window.onload = ()=>WhatAmIDoing(Math.ceil(d.getHours()/4));
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar/>
+        <Routes>  
+          <Route path='/' element={<Home currently={currently}/>} />
+          <Route path='/about' element={<About/>} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
