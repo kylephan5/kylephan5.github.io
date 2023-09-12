8cd3e6299410e6b9538764c4f8a6e0c6210a8da2
Kyle Phan <kylephan@dhcp-10-7-113-59.dhcp.nd.edu>
2023-09-12 16:32:36 -0400

    edit about

--- src/components/About.js
+++ src/components/About.js
@@ -14,9 +14,9 @@ import django from './../skillicons/django.png';
 
 export default function About() {
     const blurb = [
-        {index: 1, description: 'Rising junior studying computer science at the University of Notre Dame'},
-        {index: 2, description: 'Interested in mobile development, game development, databases, and networking'},
-        {index: 3, description: 'Currently working at iCapital, a fintech company based in NYC, as a software engineering intern'},
+        {index: 1, description: 'Junior studying computer science at the University of Notre Dame'},
+        {index: 2, description: 'Interested in mobile development, computer vision, databases, and backend development roles!'},
+        {index: 3, description: 'Previous experience at iCapital, a fintech company based in NYC, as a software engineering intern'},
         {index: 4, description: 'Catan and LA Clippers enthusiast'},
         {index: 5, description: 'Enjoys playing ping pong, eating Vietnamese food, and exercising in free time'},
     ]
